fa35b16549d4fa2a2ba4cbf5df89f530299bb0fd
AnPoryadina <an.poryadina@mail.ru>
2016-10-25 21:39:53 +0300

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,16 @@
+N = int(input())
+file = open('input2.txt')
+s = file.read().split('\n')
+tng = []
+o = {}
+for i in range(N):
+    tng.append(input())
+for i in tng:
+    for j in s:
+        if i in j.split(':')[1].split():
+            o[j] = i
+res = []
+for i in o:
+    res.append(i.split(' : '))
+for i in res:
+    print(i[0], ':', i[1])
